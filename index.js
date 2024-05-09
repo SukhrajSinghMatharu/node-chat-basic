aedbaf7248aba77ed3dc615122e9b3b17e012c95
codesolutionsin <git config --global user.email>
2024-05-10 00:57:08 +0530

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require("express")();
 var http = require("http").Server(app);
 
 const io = require("socket.io")(http);
-const port = require("dot-env").port;
+require("dotenv").config();
 
 const users = [];
 
@@ -87,6 +87,7 @@ io.on("connection", (socket) => {
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-http.listen(port, function () {
+console.log("", process.env.PORT);
+http.listen(process.env.PORT, function () {
   console.log("listening on *:3000");
 });
